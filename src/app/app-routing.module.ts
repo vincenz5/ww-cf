ff00c36a56a8fa837db13dba766ea12f9aab31c9
vincenz5 <vincenzbuhler@gmail.com>
2018-11-10 17:01:40 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from 'src/app/landing/landing.component';
+import { AboutComponent } from 'src/app/about/about.component';
+import { CrowdfundingComponent } from 'src/app/crowdfunding/crowdfunding.component';
+import { DonationComponent } from 'src/app/donation/donation.component';
+import { EducationComponent } from 'src/app/education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[{ path: '', component: LandingComponent }, 
+{ path: 'about', component: AboutComponent }, 
+{ path: 'donation', component: DonationComponent }, 
+{ path: 'crowdfunding', component: CrowdfundingComponent }, 
+{ path: 'education', component: EducationComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PanelModule, AutoCompleteModule, LayoutModule} from '@eamode/eang'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { DonationComponent } from './donation/donation.component';
+import { CrowdfundingComponent } from './crowdfunding/crowdfunding.component';
+import { EducationComponent } from './education/education.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DonationComponent,
+    CrowdfundingComponent,
+    EducationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PanelModule, AutoCompleteModule, LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
