6f2ea9c31190d3920edddc66ba45278197526c54
vincenz5 <vincenzbuhler@gmail.com>
2018-11-13 18:30:04 -0400

    Removed Web3

--- src/app/donation/GetCount.js
+++
@@ -1,9 +0,0 @@
-counter.getCount.call(function (err, result) {
-    if (err) {
-      return error(err);
-    } else {
-      log("getCount call executed successfully.");
-    }
-  
-    // Use the function's return value
-  });
--- src/app/donation/SendTransaction.js
+++
@@ -1,9 +0,0 @@
-counter.increment.sendTransaction(function (err, hash) {
-    if (err) {
-      return error(err);
-    }
-  
-    waitForReceipt(hash, function () {
-      log("Transaction succeeded.");
-    });
-  });
